882e17420624487d5d13882798a91e2b1a4d66ff
Adnan Mushtaq <madnandeveloper@gmail.com>
2026-06-27 15:46:19 +0500

    feat: improve shadow extraction

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -2,10 +2,10 @@
 from utils.file_utils import cleanup_temp_files, generate_temp_filepath, get_background_path
 from utils.image_utils import (
     composite_final_image,
-    extract_shadow_mask,
     load_and_prepare_background,
     prepare_vehicle_cutout,
 )
+from utils.shadow import extract_shadow_mask
 
 __all__ = [
     "cleanup_temp_files",
